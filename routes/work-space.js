46a927e348b9b832103ac7ba5fa719da661a93d2
Glazkov Pavel <glazprom@mail.ru>
2022-03-16 16:38:03 +0300

    feat: Сделал удаление юзера из массива пользователей

--- routes/work-space.js
+++ routes/work-space.js
@@ -1,6 +1,6 @@
 const workSpace = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const { createWorkSpace, getWorkSpace, deleteSpaceById, updateWorkSpace } = require('../controllers/work-space');
+const { createWorkSpace, getWorkSpace, deleteSpaceById, updateWorkSpace, deleteUserWorkSpace } = require('../controllers/work-space');
 
 workSpace.get('/work-space', getWorkSpace);
 
@@ -33,5 +33,13 @@ workSpace.patch('/work-space/:_id',celebrate({
   body: Joi.object().keys({
     id: Joi.string().required().length(24).hex(),
   })}), updateWorkSpace );
+
+workSpace.patch('/work-space-delete/:_id',celebrate({
+  params: Joi.object().keys({
+    _id: Joi.string().required().length(11).hex(),
+  }),
+  body: Joi.object().keys({
+    id: Joi.string().required().length(24).hex(),
+  })}), deleteUserWorkSpace );
 
 module.exports = { workSpace };
